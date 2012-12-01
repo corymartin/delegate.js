3b5cf314aa3d43ce7986197d4bc0fc3c14988642
Cory Martin <bluplastik@gmail.com>
2012-12-01 18:59:42 -0500

    Version bump

--- build/delegate.js
+++ build/delegate.js
@@ -1,7 +1,7 @@
 /*!
  * @preserve
  * Delegate.js
- * v0.1.0
+ * v0.2.0
  * Utility to delegate DOM events (>= IE8)
  * https://github.com/corymartin/delegate
  * Copyright (c) 2012 Cory Martin
@@ -14,50 +14,59 @@
 
   var previousDelegate = window.delegate;
 
-  var isLegacyEvtModel = !document.addEventListener;
+  var isLegacy = !document.addEventListener;
 
-  var addEvtMethod    = isLegacyEvtModel ? 'attachEvent' : 'addEventListener';
-  var removeEvtMethod = isLegacyEvtModel ? 'detachEvent' : 'removeEventListener';
+  var addEvtMethod    = isLegacy ? 'attachEvent' : 'addEventListener';
+  var removeEvtMethod = isLegacy ? 'detachEvent' : 'removeEventListener';
 
-  var domLoadEvt = isLegacyEvtModel
-    ? (isLegacyEvtModel ? 'on' : '') + 'readystatechange'
+  var domLoadEvt = isLegacy
+    ? (isLegacy ? 'on' : '') + 'readystatechange'
     : 'DOMContentLoaded';
 
-  // Test support for focusin/focusout events.
-  // Firefox has not implemented them yet.
+  var trimRegex             = /^\s+|\s+$/g;
+  var readyStateChangeRegex = /(on)?readystatechange/;
+
+  // Firefox has not yet implemented focusin/focusout
   //   https://bugzilla.mozilla.org/show_bug.cgi?id=687787
   var isFocusinSupported = false;
-  document[addEvtMethod](
-      domLoadEvt
-    , function testFocusin() {
-        var a = document.createElement('a');
-        a.href = '#';
-        document.body.appendChild(a);
-        a[addEvtMethod](
-            isLegacyEvtModel ? 'onfocusin' : 'focusin'
-          , function(){ isFocusinSupported = true; }
-          , false
-        );
-        a.focus();
-        document.body.removeChild(a);
-        document[removeEvtMethod](domLoadEvt, testFocusin, false);
-      }
-    , false
-  );
+  var isDOMReady         = false;
+  var domLoadEvent       = {};
 
-  var trim = function(str) {
-    return str.replace(/^\s+|\s+$/g, '');
-  };
+  /*
+   * Focusin detect, isDOMReady
+   */
+  delegate(function(evt) {
+    isDOMReady   = true;
+    domLoadEvent = evt;
+    var a = document.createElement('a');
+    a.href = '#';
+    document.body.appendChild(a);
+    a[addEvtMethod](
+        isLegacy ? 'onfocusin' : 'focusin'
+      , function(){ isFocusinSupported = true; }
+      , false
+    );
+    a.focus();
+    document.body.removeChild(a);
+  });
+
 
   var processEvtType = function(evtType) {
-    evtType = trim(evtType);
+    evtType = evtType.replace(trimRegex, '');
     if (isFocusinSupported) {
       if      (evtType === 'focus') evtType = 'focusin';
       else if (evtType === 'blur')  evtType = 'focusout';
     }
-    if (isLegacyEvtModel) evtType = 'on' + evtType;
+    if (isLegacy) evtType = 'on' + evtType;
     return evtType;
   };
+
+
+  var doCancelReadyState = function(evt) {
+    return document.readyState !== 'complete'
+      && readyStateChangeRegex.test(evt.type);
+  };
+
 
   var listenerBody = function(evt, target, selector, listener) {
     if (!selector) {
@@ -84,30 +93,51 @@
    * @api public
    */
   function delegate(target, evtType, selector, listener) {
-    if (arguments.length === 3) {
-      listener = selector;
-      selector = undefined;
+    if (arguments.length === 1) {
+      // DOM ready
+      listener = target;
+      if (isDOMReady) {
+        listener.call(document, domLoadEvent);
+        return function(){}; // noop since no event is actually added
+      }
+      evtType  = domLoadEvt;
+      target   = document;
+    }
+    else {
+      // No selector passed
+      if (arguments.length === 3) {
+        listener = selector;
+        selector = undefined;
+      }
+      evtType = processEvtType(evtType);
     }
 
-    evtType = processEvtType(evtType);
-
     var listenerProxy = function(evt) {
-      if (isLegacyEvtModel) {
+      if (isLegacy) {
         evt = window.event;
         evt.target = evt.srcElement;
       }
+      if (doCancelReadyState(evt)) return;
+
       listenerBody(evt, target, selector, listener);
+
+      if (evtType === domLoadEvt) {
+        // Remove DOM ready listener
+        remove();
+      }
+    };
+
+    var remove = function() {
+      target[removeEvtMethod](evtType, listenerProxy, true);
     };
 
     target[addEvtMethod](evtType, listenerProxy, true);
 
-    return function() {
-      target[removeEvtMethod](evtType, listenerProxy, true);
-    };
+    return remove;
   };
 
 
-  delegate.VERSION = '0.1.0';
+  delegate.VERSION = '0.2.0';
 
 
   /*
