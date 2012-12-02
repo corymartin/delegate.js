138ac4f892cdbfaba5ee93769638c9005d1eadfb
Cory Martin <bluplastik@gmail.com>
2012-12-01 21:01:40 -0500

    Inlined single use regexes

--- build/delegate.js
+++ build/delegate.js
@@ -14,61 +14,90 @@
 
   var previousDelegate = window.delegate;
 
-  var isLegacyEvtModel = !document.addEventListener;
+  var isLegacy = !document.addEventListener;
 
-  var addEvtMethod    = isLegacyEvtModel ? 'attachEvent' : 'addEventListener';
-  var removeEvtMethod = isLegacyEvtModel ? 'detachEvent' : 'removeEventListener';
+  var addEvtMethod    = isLegacy ? 'attachEvent' : 'addEventListener';
+  var removeEvtMethod = isLegacy ? 'detachEvent' : 'removeEventListener';
 
-  var domLoadEvt = isLegacyEvtModel
-    ? (isLegacyEvtModel ? 'on' : '') + 'readystatechange'
+  var domLoadEvt = isLegacy
+    ? (isLegacy ? 'on' : '') + 'readystatechange'
     : 'DOMContentLoaded';
 
-  // Test support for focusin/focusout events.
-  // Firefox has not implemented them yet.
+
+  // Firefox has not yet implemented focusin/focusout
   //   https://bugzilla.mozilla.org/show_bug.cgi?id=687787
   var isFocusinSupported = false;
-  document[addEvtMethod](
-      domLoadEvt
-    , function testFocusin() {
-        var a = document.createElement('a');
-        a.href = '#';
-        document.body.appendChild(a);
-        a[addEvtMethod](
-            isLegacyEvtModel ? 'onfocusin' : 'focusin'
-          , function(){ isFocusinSupported = true; }
-          , false
-        );
-        a.focus();
-        document.body.removeChild(a);
-        document[removeEvtMethod](domLoadEvt, testFocusin, false);
-      }
-    , false
-  );
+  var isDOMReady         = false;
+  var domLoadEvent       = {};
 
-  var trim = function(str) {
-    return str.replace(/^\s+|\s+$/g, '');
-  };
+  var anchorEl = document.createElement('a');
+  anchorEl.href = '#';
+
+  /*
+   * Focusin detect, isDOMReady
+   */
+  delegate(function(evt) {
+    isDOMReady   = true;
+    domLoadEvent = evt;
+    document.body.appendChild(anchorEl);
+    anchorEl[addEvtMethod](
+        isLegacy ? 'onfocusin' : 'focusin'
+      , function(){ isFocusinSupported = true; }
+      , false
+    );
+    anchorEl.focus();
+    document.body.removeChild(anchorEl);
+  });
+
+
+  var matches = (function() {
+    var fns = [
+        'oMatchesSelector'
+      , 'msMatchesSelector'
+      , 'mozMatchesSelector'
+      , 'webkitMatchesSelector'
+      , 'matchesSelector'
+      , 'matches'
+    ];
+    for (var i = fns.length; i--;) {
+      if (fns[i] in anchorEl) return fns[i];
+    }
+    return false;
+  })();
+
 
   var processEvtType = function(evtType) {
-    evtType = trim(evtType);
+    evtType = evtType.replace(/^\s+|\s+$/g, '');
     if (isFocusinSupported) {
       if      (evtType === 'focus') evtType = 'focusin';
       else if (evtType === 'blur')  evtType = 'focusout';
     }
-    if (isLegacyEvtModel) evtType = 'on' + evtType;
+    if (isLegacy) evtType = 'on' + evtType;
     return evtType;
   };
+
+
+  var doCancelReadyState = function(evt) {
+    return document.readyState !== 'complete'
+      && (/(on)?readystatechange/).test(evt.type);
+  };
+
 
   var listenerBody = function(evt, target, selector, listener) {
     if (!selector) {
       listener.call(target, evt);
       return;
     }
-    var t = evt.target;
+    if (matches) {
+      if (evt.target[matches](selector)) {
+        listener.call(evt.target, evt);
+      }
+      return;
+    }
     var els = target.querySelectorAll(selector);
     for (var len = els.length; len--;) {
-      if (t === els[len]) {
-        listener.call(t, evt);
+      if (evt.target === els[len]) {
+        listener.call(evt.target, evt);
         break;
       }
     }
@@ -84,26 +113,47 @@
    * @api public
    */
   function delegate(target, evtType, selector, listener) {
-    if (arguments.length === 3) {
-      listener = selector;
-      selector = undefined;
+    if (arguments.length === 1) {
+      // DOM ready
+      listener = target;
+      if (isDOMReady) {
+        listener.call(document, domLoadEvent);
+        return function(){}; // noop since no event is actually added
+      }
+      evtType  = domLoadEvt;
+      target   = document;
+    }
+    else {
+      // No selector passed
+      if (arguments.length === 3) {
+        listener = selector;
+        selector = undefined;
+      }
+      evtType = processEvtType(evtType);
     }
 
-    evtType = processEvtType(evtType);
-
     var listenerProxy = function(evt) {
-      if (isLegacyEvtModel) {
+      if (isLegacy) {
         evt = window.event;
         evt.target = evt.srcElement;
       }
+      if (doCancelReadyState(evt)) return;
+
       listenerBody(evt, target, selector, listener);
+
+      if (evtType === domLoadEvt) {
+        // Remove DOM ready listener
+        remove();
+      }
+    };
+
+    var remove = function() {
+      target[removeEvtMethod](evtType, listenerProxy, true);
     };
 
     target[addEvtMethod](evtType, listenerProxy, true);
 
-    return function() {
-      target[removeEvtMethod](evtType, listenerProxy, true);
-    };
+    return remove;
   };
 
 
